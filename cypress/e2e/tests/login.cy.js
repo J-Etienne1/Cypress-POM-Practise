e3de4b6b2c60d41332fc6ba2fe82edf21f5f2e6b
Jason <jasonetienne1@gmail.com>
2024-07-16 20:16:29 +0100

    created the test to login in and navigate to Admin and then create a new employee record

---
+++ cypress/e2e/tests/login.cy.js
@@ -0,0 +1,25 @@
+import LoginPage from "../../page-objects/LoginPage";
+import DashboardPage from "../../page-objects/DashboardPage";
+import AdminPage from "../../page-objects/AdminPage";
+
+describe("OrangeHRM Login and create employee", () => {
+    const loginPage = new LoginPage();
+    const dashboardPage = new DashboardPage();
+    const adminPage = new AdminPage();
+
+    it('should login and create a new employee record', () => {
+        // Visit the login page amd login
+        loginPage.visit();
+        loginPage.fillUsername("Admin");
+        loginPage.fillPassword("admin123");
+        loginPage.submit();
+
+        // Navigate to the Admin section
+        dashboardPage.navigateToAdmin();
+
+        // Add a new employee record
+        adminPage.clickAdd();
+        adminPage.fillEmployeeDetails("Nick","Cave", "N-Cave", "Password123!")
+        adminPage.save();
+    });
+})
