1b9236b1b456f553cc49b8f3d3ee0dd5299790e6
Jason <jasonetienne1@gmail.com>
2024-07-17 07:48:43 +0100

    add adminSelectors to selectors folder

---
+++ cypress/selectors/adminSelectors.js
@@ -0,0 +1,5 @@
+export const adminSelectors = {
+    editButton: ':nth-child(2) > .oxd-table-row > :nth-child(6) > .oxd-table-cell-actions > :nth-child(2) > .oxd-icon',
+    usernameInput: ':nth-child(2) > .oxd-input',
+    saveButton: '.oxd-button--secondary'
+};
